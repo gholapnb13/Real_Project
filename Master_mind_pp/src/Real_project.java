97ea8c99c16af3bff40453c9bd3fe961bde6a031
nilesh <nileshgholap46@gmail.com>
2022-11-20 11:12:55 +0530

    hii

---
+++ Master_mind_pp/src/Real_project.java
@@ -0,0 +1,22 @@
+
+public class Real_project {
+      
+	     public static void main(String[] args) {
+			
+	    	 int a = 4;
+
+	 		int b = 8;
+
+	 		if((b - a--)>= a)
+
+	 		{
+
+	 		if((b + a) % 2 == 1)
+
+	 		System.out.println(a*b); else
+
+	 		System.out.println(b + a);
+
+	 	}
+		}
+}
